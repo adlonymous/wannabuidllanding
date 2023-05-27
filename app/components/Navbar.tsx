3c7f0519e0e06530dcb9e2eed85c3d6ece8afa77
adlonymous <adlonymous@gmail.com>
2023-05-27 12:12:15 +0530

    Basic Setup

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <nav>
+        <h1>
+            <img src='../assets/wannabuidllogoandtype.png' alt="WannaBuidl Logo"/>
+        </h1>
+
+    </nav>
+  )
+}
+
+export default Navbar
